e62fe53ef5856a20aa431fc8ddf810316d69d050
tusharparimi <tushar.parimi@gmail.com>
2025-01-01 17:19:09 +0530

    minor change

--- examples/read_pbm_file.c
+++ examples/read_pbm_file.c
@@ -1,5 +1,6 @@
 #include "imgcodecs.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     const char *filename = 
@@ -16,8 +17,20 @@ int main(){
     printf("width: %d, height: %d", image->width, image->height);
     printf("\ndata:\n");
     for (int i = 0; i < image->width * image->height; i++){
-        printf("%c ", *(image->data + i));
+        printf("%u ", *(image->data + i));
     }
+
+    unsigned char *pixelData = img2pixelData(image);
+    if (pixelData == NULL) {
+        exit(0);
+    }
+
+    printf("\npixelated data:\n");
+    for (int i = 0; i < image->width * image->height * 3; i++){
+        printf("%u ", *(pixelData + i));
+    }
+
+    free(pixelData);
 
     return 0;
 }
