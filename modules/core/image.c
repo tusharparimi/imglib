eaca3e92ad790e55104f1a819e4c754a3bee1944
[Tushar Parimi] <tushar.parimi@gmail.com>
2025-01-10 21:30:36 +0530

    minor refactor

--- modules/core/image.c
+++ modules/core/image.c
@@ -5,15 +5,13 @@
 unsigned char *img2pixelData(Image *image) {
     int w = image->width;
     int h = image->height;
-    // printf("Here\n");
     unsigned char *pixelData = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
-    // printf("Here\n");
+
     if (pixelData == NULL) {
         free(pixelData);
         printf("Memory not allocated.\n");
         return NULL;
     }
-    // printf("Here\n");
 
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) { 
@@ -22,8 +20,6 @@ unsigned char *img2pixelData(Image *image) {
             pixelData[((y * w) + x) * 3 + 2] = image->data[(y * w) + x] * 255;
         }
     }
-    // printf("%u %u %u\n", pixelData[0], pixelData[1], pixelData[2]);
-    // printf("Here");
 
     return pixelData;
 }
