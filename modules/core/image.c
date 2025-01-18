b7998c6e50060b170a5ab844799ad4ee17fa03fc
[Tushar Parimi] <tushar.parimi@gmail.com>
2025-01-19 05:01:32 +0530

    removed debug prints

--- modules/core/image.c
+++ modules/core/image.c
@@ -33,26 +33,21 @@ unsigned char max_value) {
                 g = (data[(y * w) + x] * 255) / max_value;
                 b = (data[(y * w) + x] * 255) / max_value;
             }
-            // printf("%u ", (data[(y * w) + x] * 255)/max_value);
 
             pixelData[((y * w) + x) * 3 + 0] = r;
             pixelData[((y * w) + x) * 3 + 1] = g;
             pixelData[((y * w) + x) * 3 + 2] = b;
 
-            // printf("%u ", (pixelData[(y * w) + x]));
         }
     }
-    // printf("\n");
 
     return pixelData;
 }
 
 unsigned char *binary2pixelData(int width, int height, unsigned char *bytes) {
-    int w = width;
-    int h = height;
-    int bytes_per_row = (w + 7) / 8;
-    // int last_byte_num_pixels = (w % 8);
-    unsigned char *pixelData = (unsigned char *)malloc(w * h * 3 * sizeof(unsigned char));
+
+    int bytes_per_row = (width + 7) / 8;
+    unsigned char *pixelData = (unsigned char *)malloc(width * height * 3 * sizeof(unsigned char));
 
     if (pixelData == NULL) {
         free(pixelData);
@@ -60,35 +55,21 @@ unsigned char *binary2pixelData(int width, int height, unsigned char *bytes) {
         return NULL;
     }
 
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-
-            // printf("y: %d x: %d\n", y, x);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
 
             int byte_index = y * bytes_per_row + (x / 8);
             int bit_index = x % 8;
-            // printf("byte_index: %d bit_index: %d\n", byte_index, bit_index);
-            
             unsigned char byte = bytes[byte_index];
-            // printf("byte: %c\n", byte);
-
             unsigned char pixel = (byte >> (7 - bit_index)) & 1;
-            // printf("pixel: %d\n", pixel);
 
-            // printf("r: %d\n", ((y * w) + x) * 3 + 0);
-            // printf("g: %d\n", ((y * w) + x) * 3 + 1);
-            // printf("b: %d\n", ((y * w) + x) * 3 + 2);
             //ternary operator used to reverse 0s and 1s, in pbm  0 is white and 1 is black
-            pixelData[((y * w) + x) * 3 + 0] = (pixel == 0 ? 1 : 0) * 255;
-            pixelData[((y * w) + x) * 3 + 1] = (pixel == 0 ? 1 : 0) * 255;
-            pixelData[((y * w) + x) * 3 + 2] = (pixel == 0 ? 1 : 0) * 255;
+            pixelData[((y * width) + x) * 3 + 0] = (pixel == 0 ? 1 : 0) * 255;
+            pixelData[((y * width) + x) * 3 + 1] = (pixel == 0 ? 1 : 0) * 255;
+            pixelData[((y * width) + x) * 3 + 2] = (pixel == 0 ? 1 : 0) * 255;
 
         }
-        // printf("break\n\n\n");
     }
-
-    printf("%d, %d, %d, %d, %d, %d, %d\n", width, height, width*height, bytes_per_row, 
-    bytes_per_row*height, 8-(width % 8), w*h*3);
 
     return pixelData;
 }
@@ -102,19 +83,8 @@ unsigned char *P5_bytes2pixelData(int width, int height, unsigned char *bytes, u
         return NULL;
     }
 
-    // int i = 0;
-    // unsigned char val = 0;
-
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            
-            // val = *(bytes + i);
-            // printf("%c\n", val);
-            // if (val == ' ') {
-            //     i++;
-            //     val = *(bytes + i);
-            //     // printf("new val: %c\n", val);
-            // }
 
             pixelData[((y * width) + x) * 3 + 0] = ((bytes[(y * width) + x]) * 255) / max_value;
             pixelData[((y * width) + x) * 3 + 1] = ((bytes[(y * width) + x]) * 255) / max_value;
